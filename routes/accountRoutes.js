653b09583f46717cd0f2d0d22ea3fa449cbdddde
Pavel <pho@2021.ai>
2018-12-07 15:52:39 -0500

    added update and delete

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -7,53 +7,9 @@ const router = express.Router();
 router.use(bodyParser.json());
 
 
-
-  /**
-   * account/getall
-   * Returns all accounts for user that is logged in.
-   */
-router.get("/getall", (req, res) => {
-  console.log("/getall: NEEDS REFACTOR");
-  console.log("/account/getall: " + req.param("owner_id"));
-
-  doIfLoggedIn(req.param("owner_id"), res, (id) => {
-    accountModel.find({owner_id: id}, (accountError, accountResult) =>{
-      if(accountResult) {
-        // send accounts
-        res.json(accountResult);
-      } else {  
-        // no accounts to send
-        res.json();
-      }
-    });
-  });
-
-
-});
-
-
 /**
- * account/create
- * Create new account for user that is logged in
+ * check if user is logged in before executing any action
  */
-router.post("/create", (req, res) => {
-  console.log("/account/create: " + req.body.owner_id + " " + req.body.nickName);
-
-  doIfLoggedIn(req.body.owner_id, res, (id) => {
-    console.log("Creating..." + req.body.nickName);
-
-    accountModel.create(req.body, (err, result) => {
-      if(err){
-        console.error("Error: " + err);
-      } else {
-        console.log("Result: " + result);
-        res.status(200);
-        res.send();
-      }
-    }); // end of accountmodel.create
-  }); // end of doIfLoggedIn + callback
-}); // end of post(/create)
-
 function doIfLoggedIn(user_id, res, callback) {
   console.log("isLoggedIn(" + user_id + ")");
   if(user_id){
@@ -79,40 +35,93 @@ function doIfLoggedIn(user_id, res, callback) {
   }
 }
 
+/**
+ * account/getall
+ * Returns all accounts for user that is logged in.
+ */
+router.get("/getall", (req, res) => {
+  console.log("/getall: NEEDS REFACTOR");
+  console.log("/account/getall: " + req.param("owner_id"));
+
+  doIfLoggedIn(req.param("owner_id"), res, (id) => {
+    accountModel.find({owner_id: id}, (accountError, accountResult) =>{
+      if(accountResult) {
+        // send accounts
+        res.json(accountResult);
+      } else {  
+        // no accounts to send
+        res.json();
+      }
+    });
+  });
+});
+
 
 /**
  * account/create
  * Create new account for user that is logged in
  */
-router.post("/update"), (req, res) => {
+router.post("/create", (req, res) => {
+  console.log("/account/create: " + req.body.owner_id + " " + req.body.nickName);
+
+  doIfLoggedIn(req.body.owner_id, res, (id) => {
+    console.log("Creating..." + req.body.nickName);
+
+    accountModel.create(req.body, (err, result) => {
+      if(err){
+        console.error("Error: " + err);
+      } else {
+        console.log("Result: " + result);
+        res.status(200);
+        res.send();
+      }
+    }); // end of accountmodel.create
+  }); // end of doIfLoggedIn + callback
+}); // end of post(/create)
+
+/**
+ * account/update
+ *  update user if he is logged in
+ */
+router.post("/update", (req, res) => {
   console.log("/account/update: " + req.body.owner_id + " " + req.body.nickName);
 
+  doIfLoggedIn(req.body.owner_id, res, (id) => {
+    console.log("Updating..." + req.body.nickName);
 
-  function doIfLoggedIn(user_id, res, callback) {
-    console.log("isLoggedIn(" + user_id + ")");
-    if(user_id){
-      userModel.findByIdAndUpdate({_id: user_id}, (err, result) => {
+    accountModel.findByIdAndUpdate({_id: req.body._id}, req.body, (err, result) => {
       if(err){
-        console.error(err);
-        res.status(401);
-        res.send();
-        //check login
-      }
-  
-      if(result.loggedin === true) {
-        console.log("User is logged in!");
-        callback(user_id);
+        console.error("Error: " + err);
       } else {
-        res.status(401);
+        console.log("Result: " + result);
+        res.status(200);
         res.send();
       }
-    });
-    } else {
-      res.status(401);
-      res.send();
-    }
-  }
-}
+    }); // end of accountmodel.create
+  }); // end of doIfLoggedIn + callback
+});
 
+
+/**
+ * account/delete
+ * delete user if he is logged in
+ */
+router.post("/delete", (req, res) => {
+  console.log("/account/delete: " + req.body.owner_id + " " + req.body.nickName);
+
+  doIfLoggedIn(req.body.owner_id, res, (id) => {
+    console.log("deleting..." + req.body.nickName);
+
+    accountModel.findByIdAndDelete({_id: req.body._id}, (err, result) => {
+      if(err){
+        console.error("Error: " + err);
+      } else {
+        console.log("Result: " + result);
+        res.status(200);
+        res.send();
+      }
+    }); // end of accountmodel.create
+  }); // end of doIfLoggedIn + callback
+});
 
 module.exports = router;
